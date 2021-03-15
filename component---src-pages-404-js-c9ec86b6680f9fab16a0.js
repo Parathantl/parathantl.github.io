ab1fc25e44a9c63678f52d046bdd3fcb7a680776
parathantl <parathanlive123@gmail.com>
2021-03-16 02:45:40 +0530

    Updates

---
+++ component---src-pages-404-js-c9ec86b6680f9fab16a0.js
@@ -0,0 +1,2 @@
+(self.webpackChunkparathan=self.webpackChunkparathan||[]).push([[883],{9616:function(t,e,a){"use strict";a.r(e);var n=a(7294),l=a(7198),u=a(3751);e.default=function(t){var e=t.data,a=t.location,o=e.site.siteMetadata.title;return n.createElement(l.Z,{location:a,title:o},n.createElement(u.Z,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-c9ec86b6680f9fab16a0.js.map
